090493738642adc7c964fd302439424d9837e22d
erickaqueiroz <erickaflqueiroz@gmail.com>
2026-05-07 23:12:15 -0300

    Create Footer.js

---
+++ src/componentes/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer>
+          <p>Reconectando pessoas e natureza, uma planta por vez.</p>
+          <nav>
+              <a href="#">Instagram</a>
+              <a href="#">TikTok</a>
+          </nav>
+          <p>&copy; 2026 PlantiBox. Todos os direitos reservados.</p>
+      </footer>
+    );
+};
+
+export default Footer;
